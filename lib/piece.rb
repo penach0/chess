440cbc461554af3eb1ac1aa3b8cc412f49060364
penach0 <josemanuel.penacho@gmail.com>
2022-10-23 17:58:55 +0100

    Create Piece#move
    - Alters the position of a piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,7 +1,12 @@
 # This class represents a chess piece
 # Specific pieces are descendent from it
 class Piece
-  def initialize(position)
+  def initialize(position, color)
     @position = position
+    @color = color
+  end
+
+  def move(new_position)
+    @position = new_position
   end
 end
