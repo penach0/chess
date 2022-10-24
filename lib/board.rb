b86f16d045306175a4edacbaa93fe00b6e5a1e67
penach0 <josemanuel.penacho@gmail.com>
2022-10-24 16:52:39 +0100

    Refactor Board#create_board
    - Extract #create_line to avoid nesting iterators

--- lib/board.rb
+++ lib/board.rb
@@ -2,19 +2,23 @@
 class Board
   attr_reader :board
 
-  DARK_SQUARE = "\e[48;5;94m  \e[0m".freeze
-  LIGHT_SQUARE = "\e[48;5;179m  \e[0m".freeze
+  DARK_SQUARE = "\e[48;5;94m   \e[0m".freeze
+  LIGHT_SQUARE = "\e[48;5;179m   \e[0m".freeze
 
   def initialize(board = nil)
     @board = board || create_board
   end
 
   def create_board
-    Array.new(8) { Array.new(8) }.map.with_index do |line, index1|
-      line.map.with_index do |_square, index2|
-        coordinate_sum = index1 + index2
-        coordinate_sum.even? ? LIGHT_SQUARE : DARK_SQUARE
-      end
+    Array.new(8) { Array.new(8) }.map.with_index do |line, row_index|
+      create_line(line, row_index)
+    end
+  end
+
+  def create_line(line, row_index)
+    line.map.with_index do |_square, col_index|
+      coordinate_sum = row_index + col_index
+      coordinate_sum.even? ? LIGHT_SQUARE : DARK_SQUARE
     end
   end
 end
