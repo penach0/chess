18e10bcde073f057acf0b3ff7abfb79f2b5a8735
penach0 <josemanuel.penacho@gmail.com>
2022-11-04 15:35:03 +0100

    Create Square class
    - All elements of the board are now Square objects. This transformation
    happen during the initialization so I can still inject a board as a
    simple array of strings for testing

--- lib/board.rb
+++ lib/board.rb
@@ -1,38 +1,48 @@
 require_relative 'coordinates'
+require_relative 'square'
 
 # Represents a Chessboard
 class Board
   include Coordinates
   attr_reader :board
 
-  def initialize(board = nil)
-    @board = board || Array.new(8) { Array.new(8, '   ') }
+  def initialize(board = Array.new(8) { Array.new(8, '   ') })
+    @board = squarify_board(board)
+  end
+
+  def squarify_board(board)
+    board.map.with_index do |line, row_index|
+      squarify_line(line, row_index)
+    end
+  end
+
+  def squarify_line(line, row_index)
+    line.map.with_index do |element, col_index|
+      Square.squarify(element, row_index, col_index)
+    end
+  end
+
+  def square(coordinate)
+    board.flatten.find { |element| element.coordinate == coordinate }
+  end
+
+  def set_piece(piece, coordinate)
+    square(coordinate).update(piece)
+  end
+
+  def remove_piece(coordinate)
+    square(coordinate).clear
+  end
+
+  def move_piece(piece, start_coordinate, end_coordinate)
+    remove_piece(start_coordinate)
+    set_piece(piece, end_coordinate)
   end
 
   def print_board
-    board_background.each do |line|
+    board.each do |line|
       puts line.join
     end
-  end
-
-  def board_background
-    board.map.with_index do |line, row_index|
-      color_line(line, row_index)
-    end
-  end
-
-  def color_line(line, row_index)
-    line.map.with_index do |square, col_index|
-      coordinate_sum = row_index + col_index
-      coordinate_sum.even? ? light_square(square) : dark_square(square)
-    end
-  end
-
-  def dark_square(square)
-    "\e[1;48;5;94m#{square}\e[0m"
-  end
-
-  def light_square(square)
-    "\e[1;48;5;179m#{square}\e[0m"
+    puts
   end
 end
---
+++ lib/square.rb
@@ -0,0 +1,41 @@
+# Represents a square on the board
+class Square
+  include Coordinates
+  attr_reader :coordinate
+  attr_accessor :piece
+
+  def self.squarify(element, row, column)
+    new(element, row, column)
+  end
+
+  def initialize(piece, row, column)
+    @piece = piece
+    @coordinate = array_to_algebraic(row, column)
+    @color = color(row, column)
+  end
+
+  def update(piece)
+    self.piece = piece
+  end
+
+  def clear
+    self.piece = '   '
+  end
+
+  def color(row, column)
+    coordinate_sum = row + column
+    coordinate_sum.even? ? 'light' : 'dark'
+  end
+
+  def dark_square(piece)
+    "\e[1;48;5;94m#{piece}\e[0m"
+  end
+
+  def light_square(piece)
+    "\e[1;48;5;179m#{piece}\e[0m"
+  end
+
+  def to_s
+    @color == 'light' ? light_square(piece) : dark_square(piece)
+  end
+end
