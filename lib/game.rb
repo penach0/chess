2207a0d0294ddecded214eb76f2f22aa9a63a9a9
penach0 <josemanuel.penacho@gmail.com>
2022-11-08 14:30:24 +0100

    Create @current_player attribute for Game
    - Create Game#change_player to to switch the current_player between
    moves

--- lib/game.rb
+++ lib/game.rb
@@ -3,12 +3,13 @@ require_relative 'user_input'
 # Represents a Game
 class Game
   include UserInput
-  attr_reader :board, :black_player, :white_player
+  attr_reader :board, :black_player, :white_player, :current_player
 
   def initialize(board: nil, black_player: nil, white_player: nil)
     @board = board || Board.new
     @black_player = black_player || Player.new('black')
     @white_player = white_player || Player.new('white')
+    @current_player = white_player
   end
 
   def setup
@@ -24,5 +25,11 @@ class Game
       black_player.make_move(board, ask_coordinate(:start_square), ask_coordinate(:end_square))
       board.print_board
     end
+  end
+
+  private
+
+  def change_player
+    @current_player = (@current_player == white_player ? black_player : white_player)
   end
 end
