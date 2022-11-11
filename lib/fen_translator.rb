a16394b4c599771cea8ec7319eec954dd970877f
penach0 <josemanuel.penacho@gmail.com>
2022-11-11 18:03:13 +0100

    Finish FEN Translator
    - Finished the logic of the FEN translator, it returns an 2d array
    with the corresponding FEN letters in their right place

--- lib/fen_translator.rb
+++ lib/fen_translator.rb
@@ -1,11 +1,29 @@
 # This module will handle the translation of FEN strings
 # into full Board objects and vice-versa
 module FENTranslator
-  def fen_to_board(fen_string)
-
+  def fen_to_array(fen_string)
+    board = split_fen(fen_string)
+    build_board(board)
   end
 
   def split_fen(fen_string)
     fen_string.split('/').map { |line| line.split('') }
+  end
+
+  def spread_empty_spaces(number)
+    Array.new(number.to_i, ' ')
+  end
+
+  def build_board(board)
+    board.map do |line|
+      build_line(line)
+    end
+  end
+
+  def build_line(line)
+    line.map do |char|
+      number = char.to_i
+      number.zero? ? char : spread_empty_spaces(number)
+    end.flatten
   end
 end
