92addc3c2d3314d297bf4265ea7830fa3b744ebf
penach0 <josemanuel.penacho@gmail.com>
2022-11-13 08:44:48 +0100

    Create chess.rb
    - This file holds all the 'require_relative' so I can require it to
    any file I need without having to require multiple files at every
    file

---
+++ lib/chess.rb
@@ -0,0 +1,15 @@
+require_relative 'coordinates'
+require_relative 'directions'
+require_relative 'move_checker'
+require_relative 'fen_translator'
+require_relative 'user_input'
+require_relative 'square'
+require_relative 'player'
+require_relative 'move'
+require_relative 'piece'
+require_relative 'bishop'
+require_relative 'no_piece'
+require_relative 'board'
+require_relative 'game'
+
+
