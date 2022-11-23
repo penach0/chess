eb004f5d4b9cbe2e3b4fb116aa52f23186c2cb1a
penach0 <josemanuel.penacho@gmail.com>
2022-11-23 15:19:56 +0000

    Separate Piece Scope from Allowed Moves
    - Create methods to support a new concept of piece 'scope' which is
    just every square a piece has influence on (can move to, capture or is
    defending). This is useful for disallowing the King from moving into an
    attacked square
    - These methods shares great similarity to the already implemented
    allowed_moves and supporting methods, but it is not obvious to me as of
    now how to dry them up

--- lib/move_checker.rb
+++ lib/move_checker.rb
@@ -15,7 +15,24 @@ module MoveChecker
     backward_path + forward_path
   end
 
+  def piece_scope(path, piece)
+    piece_index = coordinate_index(path, piece.position)
+    backward = path[0, piece_index].reverse
+    forward = path[piece_index + 1, path.length]
+
+    backward_scope = scope_in_direction(backward, first_piece_index(backward))
+    forward_scope = scope_in_direction(forward, first_piece_index(forward))
+
+    backward_scope + forward_scope
+  end
+
   private
+
+  def scope_in_direction(path, first_piece_index)
+    return path if path_empty?(path)
+
+    path[0, first_piece_index + 1]
+  end
 
   def allowed_in_direction(path, first_piece_index, piece)
     return path if path_empty?(path)
