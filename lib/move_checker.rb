33b5ddd1cffcc2614e58faec9e82499e7b8e0aa2
penach0 <josemanuel.penacho@gmail.com>
2022-11-30 14:52:48 +0000

    Refactor MoveChecker
    - Took advantage of the movement logic refactor to clean up the move
    checking logic - I no longer need to verify backward paths since all
    paths are now forward

--- lib/move_checker.rb
+++ lib/move_checker.rb
@@ -3,41 +3,22 @@ require_relative 'chess'
 # the available paths given the board state
 module MoveChecker
   def piece_scope(path, piece)
-    piece_index = coordinate_index(path, piece.position.algebraic)
-    backward = path[0, piece_index].reverse
-    forward = path[piece_index + 1, path.length]
+    return path if path_empty?(path)
 
-    backward_scope = scope_in_direction(backward, first_piece_index(backward))
-    forward_scope = scope_in_direction(forward, first_piece_index(forward))
-
-    backward_scope + forward_scope
+    path[0, blocking_piece_index(path) + 1]
   end
 
   private
-
-  def scope_in_direction(path, first_piece_index)
-    return path if path_empty?(path)
-
-    path[0, first_piece_index + 1]
-  end
 
   def forbidden_move(square, piece)
     square.piece.same_color?(piece)
   end
 
-  def first_piece_index(path)
+  def blocking_piece_index(path)
     path.index(&:occupied?)
   end
 
   def path_empty?(path)
     path.all?(&:empty?)
-  end
-
-  def first_piece(path)
-    path[first_piece_index(path)].piece
-  end
-
-  def coordinate_index(path, coordinate)
-    path.index { |square| square.algebraic == coordinate }
   end
 end
