084aefc99bc6f837436152871cfda9d14bd5d537
penach0 <josemanuel.penacho@gmail.com>
2023-01-28 15:37:49 +0000

    Create Board#clone
    - Currently refactoring the way checks are handled, the idea is to
    test all moves and verify if the player is in check in the end state of
    the board and forbid those moves
    - The #clone method is a rudimentary method to clone the Board object
    allowing for a 'testing ground' to verify if a move leaves the player
    in check, without messing with the main Board object of the game

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,7 @@ require_relative 'chess'
 # Represents a Chessboard
 class Board
   include FENTranslator
+  include BoardDecoder
   attr_reader :board
 
   SIZE = 8
@@ -40,16 +41,33 @@ class Board
                           .uniq
   end
 
-  def checking_pieces(color)
-    pieces_of_color(color).find_all { |piece| piece.attacking_king?(self) }
+  def clone
+    fen_string = fen_string(board)
+
+    Board.new(board: fen_string)
+  end
+
+  def checking_piece(color)
+    checking_pieces =
+      pieces_of_color(color).find_all { |piece| piece.attacking_king?(self) }
+
+    checking_pieces[0] if checking_pieces.size > 1
   end
 
   def checking_line(color)
-    checking_pieces = checking_pieces(color)
-    return [] if checking_pieces.size > 1
+    checking_piece = checking_piece(color)
+    return [] unless checking_piece
 
     checking_pieces.first
                    .checking_line(self)
+  end
+
+  def possible_check_defense(color)
+    checking_piece = checking_piece(color)
+    king_moves = king.possible_moves(board)
+    return king_moves unless checking_piece
+
+    king_moves + square(checking_piece.position) + checking_line(color)
   end
 
   def castle(start_coordinate, end_coordinate)
