65d11b88156f27cfee565109ebe3d6cc210cfea7
penach0 <josemanuel.penacho@gmail.com>
2023-02-02 21:10:43 +0000

    Create FEN#initialize
    - Split the FEN string into fields and assign them to appropriate
    instance variables

--- lib/fen.rb
+++ lib/fen.rb
@@ -1,4 +1,11 @@
 # This class represents a FEN string
 class FEN
-  
+  attr_reader :board, :current_player
+
+  def initialize(string)
+    fields = string.split
+
+    @board = fields[0]
+    @current_player = fields[1]
+  end
 end
