c7afa4a1b165068b4537907c4bcb86a4f92c4f52
penach0 <josemanuel.penacho@gmail.com>
2023-02-03 12:38:03 +0000

    Create MoveSet#legal_moves
    - Integrated methods from MoveChecker to compute the legal moves, the
    logic is mostly the same

--- lib/move_set.rb
+++ lib/move_set.rb
@@ -10,6 +10,22 @@ class MoveSet
     @board = board
   end
 
+  def legal_moves
+    attacking.reject { |square| forbidden_move?(square, starting_position) }
+  end
+
+  def forbidden_move?(square, starting_position)
+    square.occupied?(piece.color) || moves_into_check?(square, starting_position)
+  end
+
+  def moves_into_check?(square, starting_position)
+    board_dup = board.clone
+
+    board_dup.move_piece(starting_position, square.coordinate)
+
+    board_dup.in_check?(piece.color)
+  end
+
   def attacking
     available_paths.map(&:piece_scope)
                    .flatten
