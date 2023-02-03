fce896640b7d80d939019fb6e7f6388ee5eacb0b
penach0 <josemanuel.penacho@gmail.com>
2023-02-03 09:27:27 +0000

    Move MoveChecker#piece_scope to Path
    - Helper methods were moved too

--- lib/path.rb
+++ lib/path.rb
@@ -1,9 +1,24 @@
 # This class finds and builds paths available for a piece given
 # its position and movement
 class Path
-  attr_reader :path
+  attr_reader :path, :piece
 
   def initialize(board, coordinate, direction, steps: nil)
     @path = board.path_in_direction(coordinate, direction)
+    @piece = board.piece_in(coordinate)
+  end
+
+  def piece_scope
+    return path if path_empty?(path)
+
+    path[0, blocking_piece_index(path) + 1]
+  end
+
+  def blocking_piece_index
+    path.index(&:occupied?)
+  end
+
+  def empty?
+    path.all?(&:empty?)
   end
 end
