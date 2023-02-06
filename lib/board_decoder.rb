4d49fc8a69a971f420b1cc54fab4233c6dac635b
penach0 <josemanuel.penacho@gmail.com>
2023-02-06 18:01:49 +0000

    Create BoardDecoder class functionality
    - Copied all functionality from BoardDecoder module and adapted it into
    a class

--- lib/board_decoder.rb
+++ lib/board_decoder.rb
@@ -1,3 +1,35 @@
 # This class decodes a board object into a FEN string
 class BoardDecoder
+  attr_reader :board
+
+  def self.decode(board)
+    new(board).to_fen
+  end
+
+  def initialize(board)
+    @board = board
+  end
+
+  private
+
+  def to_fen
+    board_string = array_to_string
+    join_spaces(board_string)
+  end
+
+  def array_to_string
+    board.map do |line|
+      line_to_string(line)
+    end.join('/')
+  end
+
+  def line_to_string(line)
+    line.map do |square|
+      square.piece.fen_value
+    end.join
+  end
+
+  def join_spaces(string)
+    string.gsub(/\s+/) { |spaces| spaces.size.to_s }
+  end
 end
