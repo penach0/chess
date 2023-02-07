d4a62cc88f0bc6dc742bdbed900cbc951784b134
penach0 <josemanuel.penacho@gmail.com>
2023-02-07 16:44:20 +0000

    Give functionality to GameLauncher
    - Moved the logic for launching the game previously on main to this
    class, adapting it along the way

--- lib/game_launcher.rb
+++ lib/game_launcher.rb
@@ -2,5 +2,29 @@ require_relative 'chess'
 # This class encapsulates the logic prompting the user to start a game
 # either new or from a save
 class GameLauncher
+  include UserInput
+  attr_reader :saves
+
+  def self.run
+    new.user_choice
+  end
+
+  def initialize
+    @saves = save_files
+  end
+
+  def user_choice
+    yes_or_no?(:load_game) ? Game.load(pick_save_file) : Game.new.playing
+  end
+
+  def save_files
+    remove_extension = ->(file) { file.delete_suffix('.txt') }
   
+    Dir.children('saves').map(&remove_extension)
+  end
+
+  def pick_save_file
+    display_ordered_list(save_files)
+    pick_option(save_files)
+  end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,3 @@
 require_relative 'chess'
-include UserInput
 
-def save_files
-  remove_extension = ->(file) { file.delete_suffix('.txt') }
-
-  Dir.children('saves').map { |file| remove_extension.call(file) }
-end
-
-yes_or_no?(:load_game) ? Game.load('test') : Game.new.playing
+GameLauncher.run
