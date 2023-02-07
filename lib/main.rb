e30e28950e52426cbb425e581d1852af8a3102a5
penach0 <josemanuel.penacho@gmail.com>
2023-02-07 15:10:01 +0000

    Create #save_files
    - Returns an array of the save files without extensions, the purpose is
    to separate the array from its display
    - Currently this method lives on main but I expect an object to arise
    eventual

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,10 @@
 require_relative 'chess'
 include UserInput
 
+def save_files
+  remove_extension = ->(file) { file.delete_suffix('.txt') }
+
+  Dir.children('saves').map { |file| remove_extension.call(file) }
+end
+
 yes_or_no?(:load_game) ? Game.load('test') : Game.new.playing
