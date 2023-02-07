96e0ed5c2cf2caa0bb6efc705793342e3d69ae29
penach0 <josemanuel.penacho@gmail.com>
2023-02-07 10:19:12 +0000

    Create Save#initialize and #write_to_file
    - The write to file creates the file with the FEN string describing
    the state of the game

--- lib/save.rb
+++ lib/save.rb
@@ -1,3 +1,14 @@
+require_relative 'chess'
 # This class creates a file containing a save
 class Save
+  include UserInput
+  attr_reader :fen_string
+
+  def initialize(fen_string)
+    @fen_string = fen_string
+  end
+
+  def write_to_file
+    File.write("saves/#{ask_save_name}.txt", fen_string)
+  end
 end
