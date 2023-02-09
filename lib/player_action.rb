6353a3b50d734fc0f6e8d9e3a6953225990da12f
penach0 <josemanuel.penacho@gmail.com>
2023-02-09 15:29:27 +0000

    Create PlayerAction#dispatch_action
    - This method roughly describes the logic I'm seeking to achieve

--- lib/player_action.rb
+++ lib/player_action.rb
@@ -1,5 +1,24 @@
+require_relative 'chess'
 # This class takes in an input from the player and decides an execution
 # path based on the action the user wants to perform (save, quit, move)
 class PlayerAction
-  
+  attr_reader :input
+
+  VALID_COORDINATE = /^[a-h][1-8]$/
+
+  def initialize(input, game)
+    @input = input
+    @game = game
+  end
+
+  def dispatch_action
+    case input
+    when 'save'
+      game.save
+    when 'quit'
+      game.quit
+    when VALID_COORDINATE
+      Move.validate_starting(game.board, game.current_player, input)
+    end
+  end
 end
