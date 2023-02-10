444b4653195cf632ed3d87091a23eb40e2ca6a89
penach0 <josemanuel.penacho@gmail.com>
2023-02-10 18:50:36 +0000

    Create validation methods on MoveValidator
    - The starting square validation is done by a class method so it can
    happen before the instantiation of the object so it can then be passed
    as an argument to the initialize

--- lib/move_validator.rb
+++ lib/move_validator.rb
@@ -1,5 +1,23 @@
 require_relative 'chess'
 # This class validates user input for moves
 class MoveValidator
-  
+  attr_reader :board, :player, :start
+
+  def self.valid_start?(board, player, input)
+    piece_picked = board.piece_in(input)
+
+    piece_picked.color == player.color && piece_picked.movable?(board)
+  end
+
+  def initialize(board, player, start)
+    @board = board
+    @player = player
+  end
+
+  def valid_destination?(input)
+    possible_moves = board.piece_in(start).possible_moves(board)
+    destination = board.square(input)
+
+    possible_moves.include?(destination)
+  end
 end
