d48ddcd4c9fb74b69769bc2297fcb9a7d71f2b82
penach0 <josemanuel.penacho@gmail.com>
2023-02-15 16:10:17 +0000

    Create Paths class
    - This object finds all paths given a coordinate and the board and
    stores them all in an array

---
+++ lib/paths.rb
@@ -0,0 +1,20 @@
+require_relative 'chess'
+# This class describes an array of paths available to a piece
+class Paths
+  attr_accessor :board, :coordinate, :paths
+
+  def initialize(board, coordinate)
+    @board = board
+    @coordinate = coordinate
+    @paths = find_paths
+  end
+
+  def find_paths
+    piece.directions
+         .map { |direction| Path.new(board, coordinate, direction, steps: piece.steps) }
+  end
+
+  def piece
+    board.piece_in(coordinate)
+  end
+end
