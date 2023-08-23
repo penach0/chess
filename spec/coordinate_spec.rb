088ed59760df507d2442fdb864575d31532d4942
penach0 <josemanuel.penacho@gmail.com>
2023-08-23 18:11:22 +0100

    Create other test for Coordinate.from_string

--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -2,11 +2,18 @@ require_relative '../lib/chess'
 
 describe Coordinate do
   describe '.from_string' do
-    it 'initializes a coordinate object from a algebraic notation string' do
-      algebraic_coordinate = 'a8'
-      coordinate = described_class.from_string(algebraic_coordinate)
+    it 'initializes a coordinate object from an algebraic notation string' do
+      algebraic_string = 'a8'
+      coordinate = described_class.from_string(algebraic_string)
 
       expect(coordinate).to eq(Coordinate.new(row: 0, column: 0))
+    end
+
+    it 'initializes a coordinate from a different string' do
+      algebraic_string = 'c4'
+      coordinate = described_class.from_string(algebraic_string)
+
+      expect(coordinate).to eq(Coordinate.new(row: 4, column: 2))
     end
   end
 end
