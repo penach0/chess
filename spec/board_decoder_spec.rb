4fff43e6fd0d88d0119dbedf6bdcf1e2fbd94c00
penach0 <josemanuel.penacho@gmail.com>
2023-08-24 15:20:08 +0100

    Test BoardDecoder class
    - Removed the BoardDecoder module, it performed the same function as
    the class and is no longer in use

--- spec/board_decoder_spec.rb
+++ spec/board_decoder_spec.rb
@@ -1,11 +1,15 @@
 require_relative '../lib/chess'
 
 describe BoardDecoder do
-  describe '#fen_string' do
-    subject(:board) { Board.new(board: '2b2b2/8/7b/8/8/8/B7/2B2B2') }
+  describe '.decode' do
+    let(:decode_board) { Board.new(fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR') }
 
-    it 'decodes the board into a fen string correctly' do
-      expect(fen_string(board.board)).to eq('2b2b2/8/7b/8/8/8/B7/2B2B2')
+    it 'decodes a board back into a fen string' do
+      decode_board.move_piece(Move.new(Coordinate.from_string('e2'), Coordinate.from_string('e4')))
+      board_array = decode_board.board
+      result = described_class.decode(board_array)
+
+      expect(result).to eq('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR')
     end
   end
 end
