8aab23db9653689b98ba95ed9b1d774b7e55b9c4
penach0 <josemanuel.penacho@gmail.com>
2023-08-26 22:47:34 +0100

    Test GameState#stalemate?
    - Added some constants for useful positions

--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -2,6 +2,10 @@ require 'tempfile'
 require_relative '../lib/chess'
 
 describe GameState do
+  NORMAL_POSITION = '8/8/2q5/7K/6Q1/8/3k4/8 w'
+  STALEMATE_POSITION = '4k3/4P3/4K3/8/8/8/8/8 b'
+  CHECKMATE_POSITION = '8/8/8/8/8/8/5kq1/7K w'
+
   def game_state(position)
     described_class.new(white_name: 'Morphy', 
                         black_name: 'Capablanca', 
@@ -38,6 +42,26 @@ describe GameState do
         major_piece_position = '8/8/3q4/8/1k6/8/8/5K2 w'
 
         expect(game_state(major_piece_position)).not_to be_insufficient_material
+      end
+    end
+  end
+
+  describe '#stalemate?' do
+    context 'when the currernt player still has moves' do
+      it 'is false' do
+        expect(game_state(NORMAL_POSITION)).not_to be_stalemate
+      end
+    end
+    context 'when the current player has no moves' do
+      context 'and is in check'
+        it 'is false' do
+          expect(game_state(CHECKMATE_POSITION)).not_to be_stalemate
+        end
+
+      context 'and is not in check' do
+        it 'is true' do
+          expect(game_state(STALEMATE_POSITION)).to be_stalemate
+        end
       end
     end
   end
